fe16d91c13805273e7aeb62e42f24588c996c759
Amine SGHIR <mp6099@engie.com>
2024-02-27 12:17:52 +0100

    Remove unused environment variables and update redirect in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  redirect(process.env.PRIVATE_ADMIN_URL)
+  redirect('dashboard')
   return (
     <main>
       <h1>Home</h1>
